514b1d71027b0b72d82e8d7a2808c9d7d075dc6c
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-04-18 06:39:52 -0500

    ✨ Functions to calculate mesh parameters based on area and geometry type

--- elastowaves_spectral_analysis/utils.py
+++ elastowaves_spectral_analysis/utils.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from .constants import SIDE_TO_MESH_SIZE_RATIO
+
+
 def parse_solution_identifier(geometry_type, params):
     "Returnss strign associated with run parameters"
 
@@ -9,3 +14,24 @@ def parse_solution_identifier(geometry_type, params):
     filename = geometry_type + "-" + "-".join(params_str)
 
     return filename
+
+
+def square_mesh_params_from_area(area: float):
+    "Returns square mesh parameters from area"
+    side = (area) ** 0.5
+    mesh_size = side / SIDE_TO_MESH_SIZE_RATIO
+    return {"side": side, "mesh_size": mesh_size}
+
+
+def circle_mesh_params_from_area(area: float):
+    "Returns circle mesh parameters from area"
+    radius = (area / np.pi) ** 0.5
+    mesh_size = radius / SIDE_TO_MESH_SIZE_RATIO
+    return {"radius": radius, "mesh_size": mesh_size}
+
+
+def triangle_mesh_params_from_area(area: float):
+    "Returns triangle mesh parameters from area"
+    cathethus = (2 * area) ** 0.5
+    mesh_size = cathethus / SIDE_TO_MESH_SIZE_RATIO
+    return {"cathethus": cathethus, "mesh_size": mesh_size}
