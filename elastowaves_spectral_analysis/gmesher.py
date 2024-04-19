6c3d869d12e4afcb326103703e58201176ce780b
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-04-19 11:50:20 -0500

    ✨ Add isospectral pairs in gmesher module

--- elastowaves_spectral_analysis/gmesher.py
+++ elastowaves_spectral_analysis/gmesher.py
@@ -47,7 +47,7 @@ def _create_triangle_mesh(cathetus: float, mesh_size: float, mesh_file: str):
     gmsh.initialize()
     gmsh.option.setNumber("General.Verbosity", 0)
 
-    gmsh.model.add("square")
+    gmsh.model.add("triangle")
 
     gmsh.option.setNumber("Mesh.Algorithm", 2)
     gmsh.option.setNumber("Mesh.ElementOrder", 2)
@@ -103,11 +103,123 @@ def _create_circle_mesh(radius: float, mesh_size: float, mesh_file: str):
     gmsh.finalize()
 
 
+def _create_isospectral_1_1(mesh_file: str):
+    """
+    Create a mesh for the isospectral domain presented
+    in https://en.wikipedia.org/wiki/Hearing_the_shape_of_a_drum#/media/File:Isospectral_drums.svg
+    (the left one)
+    """
+    coords = [
+        (2, 0),
+        (3, 1),
+        (3, 2),
+        (1, 2),
+        (1, 3),
+        (0, 2),
+        (1, 1),
+        (2, 1),
+    ]
+    mesh_size = 0.1
+    _create_mesh_from_coords(coords, mesh_size, mesh_file)
+
+
+def _create_isospectral_1_2(mesh_file: str):
+    """
+    Create a mesh for the isospectral domain presented
+    in https://en.wikipedia.org/wiki/Hearing_the_shape_of_a_drum
+    (the right one)
+    """
+
+    coords = [
+        (2, 0),
+        (2, 1),
+        (3, 1),
+        (2, 2),
+        (1, 2),
+        (1, 3),
+        (0, 3),
+        (0, 2),
+    ]
+    mesh_size = 0.1
+    _create_mesh_from_coords(coords, mesh_size, mesh_file)
+
+
+def _create_isospectral_2_1(mesh_file: str):
+    """
+    Create a mesh for the isospectral domain presented
+    in https://doi.org/10.1155/S1073792894000437
+    (fig4. 7_3, left one)
+    """
+    h = (3) ** 0.5 / 2
+    coords = [
+        (0.5, 0),
+        (3.5, 0),
+        (2.5, 2 * h),
+        (2, h),
+        (0, h),
+    ]
+    mesh_size = 0.1
+    _create_mesh_from_coords(coords, mesh_size, mesh_file)
+
+
+def _create_isospectral_2_2(mesh_file: str):
+    """
+    Create a mesh for the isospectral domain presented
+    in https://doi.org/10.1155/S1073792894000437
+    (fig4. 7_3, right one)
+    """
+    h = (3) ** 0.5 / 2
+    coords = [
+        (0, 0),
+        (1, 0),
+        (2.5, 3 * h),
+        (0.5, 3 * h),
+        (1, 2 * h),
+    ]
+    mesh_size = 0.1
+    _create_mesh_from_coords(coords, mesh_size, mesh_file)
+
+
+def _create_mesh_from_coords(coords, mesh_size, mesh_file):
+    """Create a mesh for a given set of coordinates"""
+    gmsh.initialize()
+    gmsh.option.setNumber("General.Verbosity", 0)
+    gmsh.model.add("custom")
+
+    gmsh.option.setNumber("Mesh.Algorithm", 2)
+    gmsh.option.setNumber("Mesh.ElementOrder", 2)
+
+    gmsh.option.setNumber("Mesh.CharacteristicLengthMin", mesh_size)
+    gmsh.option.setNumber("Mesh.CharacteristicLengthMax", mesh_size)
+
+    lc = mesh_size
+    ps = []
+    for coord in coords:
+        ps.append(gmsh.model.geo.addPoint(coord[0], coord[1], 0, lc))
+
+    lines = []
+    for i in range(len(ps)):
+        lines.append(gmsh.model.geo.addLine(ps[i], ps[(i + 1) % len(ps)]))
+
+    cl = gmsh.model.geo.addCurveLoop(lines)
+    gmsh.model.geo.addPlaneSurface([cl])
+
+    gmsh.model.geo.synchronize()
+    gmsh.model.mesh.generate(2)
+
+    gmsh.write(mesh_file)
+    gmsh.finalize()
+
+
 def create_mesh(geometry_type, params, mesh_file):
     mesh_functions = {
         "square": _create_square_mesh,
         "triangle": _create_triangle_mesh,
         "circle": _create_circle_mesh,
+        "isospectral_1_1": _create_isospectral_1_1,
+        "isospectral_1_2": _create_isospectral_1_2,
+        "isospectral_2_1": _create_isospectral_2_1,
+        "isospectral_2_2": _create_isospectral_2_2,
     }
     if geometry_type in mesh_functions:
         mesh_functions[geometry_type](**params, mesh_file=mesh_file)
